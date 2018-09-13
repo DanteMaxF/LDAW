74ecd5be5fad0bc0371e539d1ccc9b3888845dd6
DanteMaxF <A01207543@itesm.mx>
2018-09-13 11:31:02 -0500

    Finish chapter 5

--- django-MyFirstApp/mysite/mysite/settings.py
+++ django-MyFirstApp/mysite/mysite/settings.py
@@ -74,13 +74,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
