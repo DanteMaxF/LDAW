10dab2a973d425072fa88dcd97ccfbb19744ecbc
DanteMaxF <A01207543@itesm.mx>
2018-10-25 11:39:04 -0500

    Readable Streams

--- nodejs_tutorial/creating_server/app.js
+++ nodejs_tutorial/creating_server/app.js
@@ -1,5 +1,16 @@
 var http = require('http');
+var fs = require('fs');
 
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+
+myReadStream.on('data', function(chunk){
+  console.log('new chunk received');
+  console.log(chunk);
+});
+
+
+
+/*
 var server = http.createServer(function(req, res){
   console.log('request was made: '+ req.url);
   res.writeHead(200, {'Content-type': 'text/plain'});
@@ -8,3 +19,4 @@ var server = http.createServer(function(req, res){
 
 server.listen(3000, '127.0.0.1');
 console.log('Listening to port 3000')
+*/
