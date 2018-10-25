2a99d8fbd2c721abea3d10507e6ba2782c3b3e11
DanteMaxF <A01207543@itesm.mx>
2018-10-25 12:18:06 -0500

    Routing

--- nodejs_tutorial/creating_server/app.js
+++ nodejs_tutorial/creating_server/app.js
@@ -4,13 +4,20 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
   console.log('request was made: '+ req.url);
-  res.writeHead(200, {'Content-type': 'application/json'});
-  var myObj = {
-    name : 'Dante',
-    job : 'Web dev',
-    age : '21'
+  if(req.url === '/home' || req.url === '/'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  } else if (req.url === '/contact'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/contact.html').pipe(res);
+  } else if (req.url === '/api/people'){
+    var people = [{name: 'Dante', age: 21}, {name: 'Dude', age: 22}]
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(people));
+  } else{
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/404.html').pipe(res);
   }
-  res.end(JSON.stringify(myObj));
 });
 
 server.listen(3000, '127.0.0.1');
