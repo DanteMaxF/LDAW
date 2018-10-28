e4c2bca1e21742bbe626309acc7e7f0058e81d07
DanteMaxF <A01207543@itesm.mx>
2018-10-28 12:16:19 -0600

    POST and DELETE on the TODO app with AJAX

--- nodejs_tutorial/2_TODOAPP/controllers/todoController.js
+++ nodejs_tutorial/2_TODOAPP/controllers/todoController.js
@@ -1,15 +1,24 @@
+var bodyParser = require('body-parser');
+
+var data = [{item: 'get milk'}, {item: 'wal dog'}, {item: 'finish this tutorial'}];
+var urlEncondedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app){
 
   app.get('/todo', function(req, res){
-    res.render('todo')
+    res.render('todo', {todos: data})
   });
 
-  app.post('/todo', function(req, res){
-
+  app.post('/todo', urlEncondedParser, function(req, res){
+    data.push(req.body);
+    res.json(data);
   });
 
-  app.delete('/todo', function(req, res){
-
+  app.delete('/todo/:item', function(req, res){
+    data = data.filter(function(todo){
+      return todo.item.replace(/ /g, '-') !== req.params.item;
+    });
+    res.json(data);
   });
 
 };
