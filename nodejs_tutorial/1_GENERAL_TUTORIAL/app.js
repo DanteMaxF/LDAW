ddf3f436b83e82f7597dd9d9069ecc62a84343c3
DanteMaxF <A01207543@itesm.mx>
2018-10-28 08:34:03 -0600

    Template engines

--- nodejs_tutorial/1_GENERAL_TUTORIAL/app.js
+++ nodejs_tutorial/1_GENERAL_TUTORIAL/app.js
@@ -2,16 +2,25 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-  res.send('this is the homepage');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function(req, res){
-  res.send('this is the contact page');
+  res.sendFile(__dirname + '/contact.html');
 });
 
-app.get('/profile/:id', function(req, res){
-  res.send('You requested te see a profile with the id of ' + req.params.id);
+app.get('/profile/:name', function(req, res){
+  var data = {
+              age: 21,
+              job: 'web dev'
+             }
+  res.render('profile', {
+                        person: req.params.name,
+                        data: data
+                        });
 });
 
 app.listen(3000);
