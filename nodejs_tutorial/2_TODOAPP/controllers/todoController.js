a66f4d8b9c54c3fea8c549eb826d2bc0684e8dd5
DanteMaxF <A01207543@itesm.mx>
2018-10-28 19:31:24 -0600

    Link TODO with database

--- nodejs_tutorial/2_TODOAPP/controllers/todoController.js
+++ nodejs_tutorial/2_TODOAPP/controllers/todoController.js
@@ -1,4 +1,19 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// Connect to the database
+mongoose.connect('mongodb://test:testtodo1@ds243963.mlab.com:43963/tododb')
+
+// Create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: "Do my homework"}).save(function(err){
+  if (err) throw err;
+  console.log('Item saved');
+});
 
 var data = [{item: 'get milk'}, {item: 'wal dog'}, {item: 'finish this tutorial'}];
 var urlEncondedParser = bodyParser.urlencoded({extended: false});
